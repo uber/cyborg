3d5fff8d9ad4cb3c47d14cccfa544ba734364a0d
Ben Pious <benpious@uber.com>
2018-08-08 15:56:46 -0700

    add sample app, rename old stuff

---
+++ Cyborg/Cyborg.h
@@ -0,0 +1,15 @@
+//
+//  Copyright © Uber Technologies, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cyborg.
+FOUNDATION_EXPORT double CyborgVersionNumber;
+
+//! Project version string for Cyborg.
+FOUNDATION_EXPORT const unsigned char CyborgVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cyborg/PublicHeader.h>
+
+
---
+++ SampleApp/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  Copyright © Uber Technologies, Inc. All rights reserved.
+//
+
+import UIKit
+import Cyborg
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let drawableData = [
+            done,
+            work,
+            visibility,
+            baselinesplit,
+            androidDocsSampleTriangle,
+            person,
+            swapHorizontal,
+            translate,
+            timeline,
+            power,
+            ]
+            .map { (data) in
+                data.data(using: .utf8)!
+        }
+        var lastAnchor = view.leadingAnchor
+        for data in drawableData {
+            let vectorView = VectorView(frame: .zero)
+            view.addSubview(vectorView)
+            vectorView.translatesAutoresizingMaskIntoConstraints = false
+            VectorDrawable
+                .create(from: data) { (result) in
+                    switch result {
+                    case .ok(let drawable):
+                        vectorView.drawable = drawable
+                    case .error(let error):
+                        print(error)
+                        fatalError(error)
+                    }
+            }
+            NSLayoutConstraint
+                .activate([
+                    vectorView.leadingAnchor.constraint(equalTo: lastAnchor, constant: 8),
+                    vectorView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+                    ])
+            lastAnchor = vectorView.trailingAnchor
+        }
+        
+    }
+
+}
