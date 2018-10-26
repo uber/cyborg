5f9593ff308ba04ac7ba539d2a15cc95eb66f4b2
Ben Pious <benpious@uber.com>
2018-10-25 17:30:20 -0700

    cleanup

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -18,17 +18,26 @@ class Theme: Cyborg.ValueProviding {
 }
 
 class ViewController: UIViewController {
+
     override func viewDidLoad() {
         super.viewDidLoad()
         let drawableData = [
-            spain,
+            test,
         ]
         .map { data in
             data.data(using: .utf8)!
         }
-        for data in drawableData {
+        let scrollView = UIScrollView()
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 8
+        stackView.layoutMargins = .init(top: 20, left: 16, bottom: 0, right: 16)
+        stackView.isLayoutMarginsRelativeArrangement = true
+        view.addFullSizeSubview(scrollView)
+        scrollView.addFullSizeSubview(stackView)
+        scrollView.alwaysBounceVertical = true
+        let views = drawableData.map { data -> VectorView in
             let vectorView = VectorView(externalValues: Theme())
-            view.addSubview(vectorView)
             vectorView.translatesAutoresizingMaskIntoConstraints = false
             let result = VectorDrawable.create(from: data)
             switch result {
@@ -36,8 +45,8 @@ class ViewController: UIViewController {
                 vectorView.drawable = drawable
                 NSLayoutConstraint
                     .activate([
-                        vectorView.widthAnchor.constraint(equalToConstant: vectorView.intrinsicContentSize.width * 30),
-                        vectorView.heightAnchor.constraint(equalToConstant: vectorView.intrinsicContentSize.height * 30),
+                        vectorView.widthAnchor.constraint(equalToConstant: vectorView.intrinsicContentSize.width),
+                        vectorView.heightAnchor.constraint(equalToConstant: vectorView.intrinsicContentSize.height),
                     ])
                 return vectorView
             case .error(let error):
@@ -45,5 +54,26 @@ class ViewController: UIViewController {
                 fatalError(error)
             }
         }
+        for view in views {
+            stackView.addArrangedSubview(view)
+        }
     }
+
+}
+
+extension UIView {
+
+    func addFullSizeSubview(_ subview: UIView) {
+        addSubview(subview)
+        subview.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint
+            .activate([
+                subview.trailingAnchor.constraint(equalTo: trailingAnchor),
+                subview.leadingAnchor.constraint(equalTo: leadingAnchor),
+                subview.topAnchor.constraint(equalTo: topAnchor),
+                subview.bottomAnchor.constraint(equalTo: bottomAnchor),
+            ])
+
+    }
+
 }
