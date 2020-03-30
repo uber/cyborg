ffc7423737dcc38088bd6000d5e6c6dccd706893
benpious <benpious@gmail.com>
2020-03-30 11:42:55 -0400

    update gitignore

---
+++ Package.swift
@@ -0,0 +1,40 @@
+// swift-tools-version:5.2
+
+//
+//  Copyright (c) 2018. Uber Technologies
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//  http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+
+
+import PackageDescription
+
+let package = Package(
+    name: "Cyborg",
+    platforms: [
+        .iOS("11.0.0")
+    ],
+    products: [
+    .library(name: "Cyborg",
+             targets: ["Cyborg"])
+    ],
+    targets: [
+        .target(
+            name: "Cyborg",
+            path: "Cyborg"),
+        .testTarget(
+            name: "CyborgTests",
+            dependencies: ["Cyborg"],
+            path: "CyborgTests"),
+    ]
+)
